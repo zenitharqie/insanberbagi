1b5ec3e202d84b8dcf45a9665782c379fbdd854b
aryapras22 <88756038+aryapras22@users.noreply.github.com>
2024-05-14 13:09:24 +0700

    v2

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:insanberbagi/screen/login_screen.dart';
+import 'package:insanberbagi/user_auth/firebase_auth/firebase_auth_service.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
@@ -21,13 +23,15 @@ class Profile extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const ProfileScreen(),
+      home: ProfileScreen(),
     );
   }
 }
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  ProfileScreen({Key? key}) : super(key: key);
+
+  FirebaseAuthService _firebaseAuthService = FirebaseAuthService();
 
   @override
   Widget build(BuildContext context) {
@@ -49,56 +53,75 @@ class ProfileScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              const SizedBox(height: 40),
-              CircleAvatar(
-                radius: 60,
-                backgroundImage: AssetImage('assets/images/insanberbagi.png'),
-              ),
-              const SizedBox(height: 20),
-              itemProfile('Name', 'Zinedine Zenith Arqie Fomalhaut',
-                  CupertinoIcons.person),
-              const SizedBox(height: 10),
-              itemProfile('Phone', '082117201836', CupertinoIcons.phone),
-              const SizedBox(height: 10),
-              itemProfile(
-                  'Address', 'Mojoarum, Surabaya', CupertinoIcons.location),
-              const SizedBox(height: 10),
-              itemProfile(
-                  'Email', 'zenithzinedine1@gmail.com', CupertinoIcons.mail),
-              const SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.all(15),
-                  ),
-                  child: const Text('Edit Profile'),
-                ),
-              ),
-              SizedBox(height: 20),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-  onPressed: () {
-    Navigator.pushAndRemoveUntil(
+          child: FutureBuilder(
+              future: _firebaseAuthService.getUserDetails(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                }
+
+                var data = snapshot.data as dynamic;
+
+                return Column(
+                  children: [
+                    const SizedBox(height: 40),
+                    CircleAvatar(
+                      radius: 60,
+                      backgroundImage:
+                          AssetImage('assets/images/insanberbagi.png'),
+                    ),
+                    const SizedBox(height: 20),
+                    itemProfile(
+                        'Name', data['username'], CupertinoIcons.person),
+                    const SizedBox(height: 10),
+                    itemProfile('Phone', data['phone'], CupertinoIcons.phone),
+                    const SizedBox(height: 10),
+                    itemProfile(
+                        'Address', data['address'], CupertinoIcons.location),
+                    const SizedBox(height: 10),
+                    itemProfile('Email', data['email'], CupertinoIcons.mail),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.all(15),
+                        ),
+                        child: const Text('Edit Profile'),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushAndRemoveUntil(
                             context,
-                            MaterialPageRoute(builder: (context) => LoginPage()),
+                            MaterialPageRoute(
+                                builder: (context) => LoginPage()),
                             (route) => false,
                           );
-  },
-  style: ElevatedButton.styleFrom(
-    padding: const EdgeInsets.all(15),
-  ),
-  child: const Text('Log Out'),
-),
-
-              ),
-            ],
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.all(15),
+                        ),
+                        child: const Text('Log Out'),
+                      ),
+                    ),
+                  ],
+                );
+              }
           ),
         ),
       ),
@@ -111,13 +134,31 @@ class ProfileScreen extends StatelessWidget {
         color: Colors.green,
         borderRadius: BorderRadius.circular(10),
       ),
-      child: ListTile(
-        title: Text(title),
-        subtitle: Text(subtitle),
-        leading: Icon(iconData),
-        trailing: Icon(Icons.arrow_forward, color: Colors.grey.shade400),
-        tileColor: Colors.white,
+      child: GestureDetector(
+
+        
+        child: ListTile(
+          title: Text(title),
+          subtitle: Text(subtitle),
+          leading: Icon(iconData),
+          trailing: Icon(Icons.arrow_forward, color: Colors.grey.shade400),
+          tileColor: Colors.white,
+        ),
       ),
     );
+  }
+
+  void signOut(BuildContext context) {
+    _firebaseAuthService.signOut();
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+      (route) => false,
+    );
+  }
+
+  // update item on profile
+  void updateItem(String item) {
+    // pop up dialog field to update item
   }
 }
--- lib/user_auth/firebase_auth/firebase_auth_service.dart
+++ lib/user_auth/firebase_auth/firebase_auth_service.dart
@@ -1,12 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class FirebaseAuthService{
+class FirebaseAuthService {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<User?> singupWithEmailAndPassword(String email, String password) async {
-
+  Future<User?> singupWithEmailAndPassword(
+      String email, String password) async {
     try {
-      UserCredential credential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      UserCredential credential = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
       return credential.user;
     } catch (e) {
       print("Error");
@@ -14,16 +16,45 @@ class FirebaseAuthService{
     return null;
   }
 
-    Future<User?> singinWithEmailAndPassword(String email, String password) async {
+  // Future<User?> singinWithEmailAndPassword(
+  //     String email, String password) async {
+  //   try {
+  //     UserCredential credential = await _auth.createUserWithEmailAndPassword(
+  //         email: email, password: password);
+  //     return credential.user;
+  //   } catch (e) {
+  //     print("Error");
+  //   }
+  //   return null;
+  // }
 
-    try {
-      UserCredential credential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-      return credential.user;
-    } catch (e) {
-      print("Error");
-    }
-    return null;
+  // get current user uid
+  String? getCurrentUserUid() {
+    return _auth.currentUser!.uid;
   }
 
+  // signout
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+
+  // getall data from users storage that have uid == current user
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUserDetails() async {
+    return await FirebaseFirestore.instance
+        .collection("users")
+        .doc(getCurrentUserUid())
+        .get();
+  }
+
+  Future<void> changeUserData(String field, newValue) async {
+    await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(getCurrentUserUid())
+        .update({
+      field: newValue,
+    });
+  }
+
+  // update user data
 }
-
